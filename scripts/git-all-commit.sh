93a44932e5cb163a6ba64068a0c3e14f5389a8a4
Harsha-S2604 <harsha91148@outlook.com>
2022-05-15 04:23:58 +0000

    added a comments for scripts

--- scripts/git-all-commit.sh
+++ scripts/git-all-commit.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+<<com
+    This script is to commit and push all files.
+    Please use this script only to commit and push all files.
+    Usage:
+        ./scripts/git-all-commit.sh <commit-message> <branch-name>
+        This script takes two mandatory positional parameters
+        "$1" => message to commit
+        "$2" => remote branch name to push
+com
+
 git add .
 git commit -m "$1"
 git push origin $2
